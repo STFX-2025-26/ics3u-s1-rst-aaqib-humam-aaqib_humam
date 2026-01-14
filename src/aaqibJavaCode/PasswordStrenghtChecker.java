1c7218a6dad0198ade9ffcc81bd855ed9dc7ad5f
s490282 <s490282@FXH-5650597896>
2026-01-14 08:10:52 -0500

    main class

---
+++ src/aaqibJavaCode/PasswordStrenghtChecker.java
@@ -0,0 +1,19 @@
+package aaqibJavaCode;
+
+public class PasswordStrenghtChecker {
+
+	public static void main(String[] args) {
+		
+		
+		// variables
+		String Password = "";
+		boolean validLength = true;
+		boolean hasletters = true;
+		boolean hasNumbers = true;
+		boolean hasSpecialChar = true;
+		boolean isStrong = true;
+		
+		
+	}
+
+}
