30c9ceea2014e58992589315e7f886e4dd44d4c9
s490282 <s490282@FXH-5650597896>
2026-01-14 08:41:21 -0500

    yes

--- src/aaqibJavaCode/PasswordStrenghtChecker.java
+++ src/aaqibJavaCode/PasswordStrenghtChecker.java
@@ -1,21 +1,44 @@
 package aaqibJavaCode;
-
+import java.util.Scanner;
+import aaqibJavaCode.StringLength;
 public class PasswordStrenghtChecker {
 
 	public static void main(String[] args) {
 		
 		
 		// variables
-		String Password = "";
+		String password = "";
 		boolean validLength = true;
 		boolean hasletters = true;
 		boolean hasNumbers = true;
 		boolean hasSpecialChar = true;
 		boolean isStrong = true;
+		Scanner sc = new Scanner (System.in);
 		
 		// user input
 		
+		try {
+			
+			password = sc.next();
+			
+		}
 		
+		catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+		//length checker
+		
+		validLength = StringLength.lengthCheck(password);
+		
+		if (validLength == true) {
+			
+			//check if password contains letters
+			System.out.println("yes");
+			
+			
+		}
 		
 	}
 
