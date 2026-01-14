33600eb8cb0c06196a5172808987ec0b88478527
s490282 <s490282@FXH-5650597896>
2026-01-14 09:25:35 -0500

    completed the program and all methods

--- src/aaqibJavaCode/ContainsIntegers.java
+++ src/aaqibJavaCode/ContainsIntegers.java
@@ -1,5 +1,20 @@
 package aaqibJavaCode;
-
 public class ContainsIntegers {
 
+	
+	public static boolean hasNumbers (String a) {
+		
+		if (a.matches(".*\\d+.*")) {
+			
+			return true;
+		}
+		
+		else {
+			
+			return false;
+		}
+			
+		
+	}
+	
 }
--- src/aaqibJavaCode/ContainsSpecChars.java
+++ src/aaqibJavaCode/ContainsSpecChars.java
@@ -2,4 +2,20 @@ package aaqibJavaCode;
 
 public class ContainsSpecChars {
 
+	public static boolean hasSpecChars ( String a) {
+		
+		if (a.matches(".*[!@#$%^&*(),.?\":{}|<>].*")) {
+			
+			return true;
+			
+		}
+		
+		else {
+			
+			return false;
+			
+		}
+		
+	}
+	
 }
