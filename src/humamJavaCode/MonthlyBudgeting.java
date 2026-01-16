419880e54910ccfe03b0b0a268e1d2b8fb3fb456
s503175 <s503175@FXH-6326323414>
2026-01-16 08:57:33 -0500

    I completed the code, AAqib said it was good and I completeed the test plan and everything wnet well

--- src/humamJavaCode/MonthlyBudgeting.java
+++ src/humamJavaCode/MonthlyBudgeting.java
@@ -15,11 +15,7 @@ public class MonthlyBudgeting {
 		double totalExpenses = 0;
 		double balance = 0;
 		double yearlySavings = 0;
-		boolean incomeInteger = true;
 		boolean expenseInteger = true;
-		boolean incomePositive = true;
-		boolean expensePositive = true;
-		boolean balancePositive = true;
 		Scanner sc = new Scanner(System.in);
 		
 		
@@ -34,8 +30,146 @@ public class MonthlyBudgeting {
 		catch (Exception e) {
 			
 			System.out.println("Invalid Entry");
+			sc.next();
+			return;
 		}
-		method2(monthlyIncome);
+		expenseInteger = positiveInt(monthlyIncome);
+		if (expenseInteger == true) {
+			
+		}
+		else {
+			System.out.println("Invalid Entry");
+			return;
+		}
+		
+		System.out.println("How much do you pay for rent every month?");
+		try {
+			
+			rent = sc.nextDouble();
+		}
+		
+		catch (Exception e1) {
+			
+			System.out.println("Invalid Entry");
+			sc.next();
+			return;
+		}
+		
+		expenseInteger = positiveInt(rent);
+		if (expenseInteger == true) {
+			
+		}
+		else {
+			System.out.println("Invalid Entry");
+			return;
+		}
+		
+		System.out.println("How much do you pay for food every month?");
+		
+		try {
+			
+			food = sc.nextDouble();
+		}
+		
+		catch (Exception e2) {
+			
+			System.out.println("Invalid Entry");
+			sc.next();
+			return;
+		}
+		
+		expenseInteger = positiveInt(food);
+		if (expenseInteger == true) {
+			
+		}
+		else {
+			System.out.println("Invalid Entry");
+			return;
+		}
+		
+		System.out.println("How much do you pay for transportation every month?");
+		try {
+			
+			transpo = sc.nextDouble();
+		}
+		
+		catch (Exception e3) {
+			
+			System.out.println("Invalid Entry");
+			sc.next();
+			return;
+		} 
+		
+		expenseInteger = positiveInt(transpo);
+		if (expenseInteger == true) {
+			
+		}
+		else {
+			System.out.println("Invalid Entry");
+		}
+		
+		System.out.println("How much do you pay for entertainmnet every month?");
+		try {
+			
+			entertain = sc.nextDouble();
+			
+		}
+		
+		catch (Exception e4) {
+			
+			System.out.println("Invalid Entry");
+			sc.next();
+			return;
+		}
+		
+		expenseInteger = positiveInt(entertain);
+		if (expenseInteger == true) {
+			
+		}
+		else {
+			System.out.println("Invalid Entry");
+			return;
+		}
+		
+		System.out.println("How much do you pay for ____ every month?");
+		try {
+			
+			misc = sc.nextDouble();
+		}
+		
+		catch (Exception e5) {
+			
+			System.out.println("Invalid Entry");
+			sc.next();
+			return;
+		}
+		
+		expenseInteger = positiveInt(misc);
+		if (expenseInteger == true) {
+			
+		}
+		else {
+			System.out.println("Invalid Entry");
+			return;
+		}
+		
+		
+		totalExpenses = rent + food + transpo + entertain + misc;
+		balance = monthlyIncome - totalExpenses;
+		
+		expenseInteger = positiveInt(balance);
+		if(expenseInteger == true) {
+			yearlySavings = balance * 12;
+			System.out.println("Your monthly income is: $" + monthlyIncome);
+			System.out.println("Your balance is: $" + balance);
+			System.out.println("You are saving $" + Math.abs(balance) + " every month");
+			System.out.println("You are saving $" + Math.abs(yearlySavings) + " every year");
+		}
+		else {
+			System.out.println("Your monthly income is: $" + monthlyIncome);
+			System.out.println("Your balance is: $" + balance);
+			System.out.println("You overspent by: $" + Math.abs(balance));
+		}
 
 	}
 	
@@ -43,13 +177,14 @@ public class MonthlyBudgeting {
 	
 	
 	
-	public static void method2(double a) {
+	public static boolean positiveInt(double a) {
 		
-		if (a< 0)
-			System.out.println("Invalid Input");
+		if (a >=  0)
+			return true;
+		else
+			return false;
 	}
-	
-	
+
 
 
 }
